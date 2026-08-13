5068aeae88bd259f74aa4c80e3e5933c72e4197c
Alex Xiang <axiang1307@gmail.com>
2026-08-13 08:53:25 -0400

    transfer to work

--- lettuce_api/src/db/profiles.ts
+++ lettuce_api/src/db/profiles.ts
@@ -1,8 +1,15 @@
 import pool from '../lib/db';
-import { Profile } from '../types/index';
+import { Profile, ProfileUpdate } from '../types/index';
 export const getMe = async(userId: string): Promise<Profile | null> => {
     const result = await pool.query(
         'SELECT * FROM profiles WHERE id = $1', [userId]
     );
     return result.rows[0] ?? null;
+}
+
+export const patchMe = async(userId: string, profile: ProfileUpdate): Promise<Profile | null> => {
+    
+    const result = await pool.query(
+        'UPDATE profiles '
+    )
 }
--- lettuce_api/src/types/index.ts
+++ lettuce_api/src/types/index.ts
@@ -5,3 +5,9 @@ export type Profile = {
     id: string,
     created_at: Date
 }
+
+export type ProfileUpdate = {
+    username: string | null,
+    full_name: string | null,
+    avatar_url: string | null
+}
